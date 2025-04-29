8297e519f8d056daf0eadf432459626d0aee7956
Sanjay Singh <sanjaygurjar741@gmail.com>
2025-04-29 10:31:39 +0530

    win api explored

---
+++ winapi/consolebuffer.cpp
@@ -0,0 +1,31 @@
+#include <Windows.h>
+#include <iostream>
+
+int main()
+{
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+    char *data = "hello world";
+    DWORD characters = 0; // characters written
+
+    if (1)
+    {
+        // version 1
+
+        WriteConsoleOutputCharacter(h, data, strlen(data), {}, &characters);
+    }
+    else
+    {
+        // version 2
+        // This handle has to be closed with closeHandle(); if you don't want to leak memory.
+        // I'm not closing it now because it will be used for the entire continuation of the program.
+
+        HANDLE consolebuffer = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, nullptr,
+                                                         CONSOLE_TEXTMODE_BUFFER, 0);
+        SetConsoleActiveScreenBuffer(consolebuffer);
+
+        WriteConsoleOutputCharacter(h, data, strlen(data), {}, &characters);
+    }
+
+    std::cin.get();
+    return 0;
+}
---
+++ winapi/day5.cpp
@@ -0,0 +1,129 @@
+#include <Windows.h>
+#include <string>
+#include <vector>
+#include <iostream>
+
+struct Test
+{
+    float a = 0;
+    int b = 0;
+};
+
+size_t getFileSize(std::string name);
+bool readEntireFileBinary(std::string name, std::vector<unsigned char> &data);
+bool writeEntireFileBinary(std::string name, void *buffer, size_t s);
+
+int main()
+{
+
+    Test t;
+    t.a = 10.8;
+    t.b = 5;
+
+    // FILE_PATH macro is defined from cmake and it is an absolute path, used for convinience
+    writeEntireFileBinary(FILE_PATH "test.bin", &t, sizeof(t));
+
+    t = {};
+
+    std::vector<unsigned char> data;
+    readEntireFileBinary(FILE_PATH "test.bin", data);
+
+    if (sizeof(t) == data.size())
+        std::memcpy(&t, data.data(), sizeof(t));
+
+    std::cout << t.a << " " << t.b << "  file size bytes: " << getFileSize(FILE_PATH "test.bin");
+    std::cin.get();
+    return 0;
+}
+
+size_t getFileSize(std::string name)
+{
+    HANDLE file = CreateFile(name.c_str(), GENERIC_READ, FILE_SHARE_READ,
+                             NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+
+    if (file == INVALID_HANDLE_VALUE)
+    {
+        // log error here
+        return 0;
+    }
+
+    LARGE_INTEGER fileSize;
+    if (!GetFileSizeEx(file, &fileSize))
+    {
+        CloseHandle(file);
+        return 0;
+    }
+
+    CloseHandle(file);
+    return fileSize.QuadPart;
+}
+
+bool readEntireFileBinary(std::string name, std::vector<unsigned char> &data)
+{
+
+    data.clear();
+
+    HANDLE file = CreateFile(name.c_str(), GENERIC_READ, FILE_SHARE_READ,
+                             NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+
+    if (file == INVALID_HANDLE_VALUE)
+    {
+        // log error here
+        return false;
+    }
+
+    LARGE_INTEGER fileSize;
+    if (!GetFileSizeEx(file, &fileSize))
+    {
+        CloseHandle(file);
+        return 0;
+    }
+
+    data.resize(fileSize.QuadPart);
+
+    DWORD bytesRead = 0;
+    bool rez = ReadFile(file, data.data(), fileSize.QuadPart, &bytesRead, nullptr);
+
+    CloseHandle(file);
+
+    if (!rez || bytesRead != fileSize.QuadPart)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+bool writeEntireFileBinary(std::string name, void *buffer, size_t s)
+{
+    // no share mode because we are modifying it
+    HANDLE file = CreateFile(name.c_str(), GENERIC_WRITE, 0,
+                             NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+
+    if (file == INVALID_HANDLE_VALUE)
+    {
+        // log err
+        return false;
+    }
+
+    if (!buffer || s == 0)
+    {
+        // no file content nothing to write
+        CloseHandle(file);
+        return true;
+    }
+
+    DWORD bytesWritten = 0;
+    bool success = WriteFile(file, buffer, s, &bytesWritten, NULL);
+
+    if (bytesWritten != s)
+    {
+        success = false;
+    }
+
+    CloseHandle(file);
+
+    return success;
+}
